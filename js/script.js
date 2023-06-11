510bcc0fbb749b4c87d38998ca2c523a8f6dfb7d
Sarah Quinney <quinney.sarah@gmail.com>
2023-06-11 13:07:18 -0500

    play again button refreshes the page

--- js/script.js
+++ js/script.js
@@ -7,11 +7,10 @@ let gameboard = [
 
 //player variable//
 let player = 1;
-let squareMarker = "";
 
 //Access DOM elements//
 let squares = document.querySelectorAll(".square");
-let restart = document.querySelector("#restart");
+let playAgain = document.querySelector("#restart");
 let message = document.querySelector("#message");
 
 //add event listener for each square//
@@ -56,10 +55,47 @@ function writeMarker() {
         for (let column = 0; column < 3; column++)
             if (gameboard[row][column] == 1) {
                 //update square with an X by using inverse operations and innerText//
-                squares[(row * 3) + column].innerText="X";
+                squares[(row * 3) + column].innerText = "X";
             } else if (gameboard[row][column] == -1) {
                 //update square with an O//
-                squares[(row * 3) + column].innerText="O";
+                squares[(row * 3) + column].innerText = "O";
             }
+        //this function calls winner function to be written on the screen//
+        winner();
     }
 }
+
+//function will check the results of the gameboard//
+function winner() {
+    //this will check rows and columns//
+    for (let i = 0; i < 3; i++) {
+        let rowSum = gameboard[i][0] + gameboard[i][1] + gameboard[i][2];
+        let columnSum = gameboard[0][i] + gameboard[1][i] + gameboard[2][i];
+        if (rowSum == 3 || columnSum == 3) {
+            message.innerText = "Player One Wins!";
+            //console.log("Player One Wins!")
+        } else if (rowSum == -3 || columnSum == -3) {
+            message.innerText = "Player Two Wins!";
+            //console.log("Player Two Wins!")
+        }
+    }
+    //this will check diagonals//
+    let diagonalSumOne = gameboard[0][0] + gameboard[1][1] + gameboard[2][2];
+    let diagonalSumTwo = gameboard[0][2] + gameboard[1][1] + gameboard[2][0];
+    if (diagonalSumOne == 3 || diagonalSumTwo == 3) {
+        message.innerText = "Player One Wins!";
+        //console.log("Player One Wins!")
+    } else if (diagonalSumOne == -3 || diagonalSumTwo == -3) {
+        message.innerText = "Player Two Wins!";
+        //console.log("Player Two Wins!")
+    }
+}
+
+
+
+//refreshes page to start a new game//
+//used JQuery to practice//
+$("#restart").click(function () {
+    document.location.reload(true);
+});
+
