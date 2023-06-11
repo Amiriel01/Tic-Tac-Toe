7fb21d1277fa78763e89f03235b5b3f61b587169
Sarah Quinney <quinney.sarah@gmail.com>
2023-06-11 11:28:21 -0500

    markers are applied to board when square is clicked

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ let gameboard = [
 
 //player variable//
 let player = 1;
+let squareMarker = "";
 
 //Access DOM elements//
 let squares = document.querySelectorAll(".square");
@@ -14,6 +15,7 @@ let restart = document.querySelector("#restart");
 let message = document.querySelector("#message");
 
 //add event listener for each square//
+//index points to a number//
 squares.forEach((square, index) => {
     square.addEventListener("click", () => {
         //checks to be sure the squares are working when clicked should give a dif number for each square//
@@ -32,13 +34,32 @@ function playerMarker(index) {
     let row = (index - column) / 3
     //check to see row and column numbers//
     //console.log(gameboard);//
-    //this will check if the current square is empty before swapping players//
+    //this will check if the current square is empty before marker is put and swap players back and forth//
     if (gameboard[row][column] == 0) {
         //this will assign a square to a player click//
         gameboard[row][column] = player;
         //change player to opposite player//
         player *= -1;
         //check to see if gameboard works//
-        console.log(gameboard);
+        //console.log(gameboard);//
+        //this function calls player marker function to be written on screen//
+        writeMarker();
+    }
+}
+
+//player marker function//
+function writeMarker() {
+    //looks over rows//
+    for (let row = 0; row < 3; row++) {
+        //defined marker variables//
+        //looks over columns//
+        for (let column = 0; column < 3; column++)
+            if (gameboard[row][column] == 1) {
+                //update square with an X by using inverse operations and innerText//
+                squares[(row * 3) + column].innerText="X";
+            } else if (gameboard[row][column] == -1) {
+                //update square with an O//
+                squares[(row * 3) + column].innerText="O";
+            }
     }
 }
